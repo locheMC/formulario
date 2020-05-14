ad8a461f0ee0ed5d38bd87e49209f2ff90a1b2ae
CristianJulio <55302815+CristianJulio@users.noreply.github.com>
2020-05-14 15:47:44 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,7 +1,6 @@
 <?php
-include("datos.php");
  $con = new MySQLi("localhost","root","","registro2");
- $sql = ("insert into registro2 (Cedula,Nombre,Apellidos,Direccion,Telefono,Selecciones,Sexo)VALUES($_POST[Cedula],'$_POST[Nombre]','$_POST[Apellidos]','$_POST[Direccion]',$_POST[Telefono],'$_POST[Selecciones]','$_POST[Sexo]')");
+ $sql = ("insert into registro2(Cedula,Nombre,Apellidos,Direccion,Telefono,Selecciones,Sexo) values($_POST[Cedula],'$_POST[Nombre]','$_POST[Apellidos]','$_POST[Direccion]',$_POST[Telefono],'$_POST[Selecciones]','$_POST[Sexo]')");
  $e = $con->Query($sql);
  if($e) {
    echo "Datos ingresados";
