a3a7c18eecc8b7a459f7fef0cc111f2fe0cca60c
Jose Gonzalez <jagonzalezvillar@gmail.com>
2020-05-14 00:16:44 -0500

    Formulario

---
+++ formulario.php
@@ -0,0 +1,15 @@
+<?php
+include("enviar.php");
+if(isset($_POST['Cedula']) && !empty($_POST['Cedula']) &&
+(isset($_POST['Nombre']) && !empty($_POST['Nombre']) &&
+(isset($_POST['Apellido']) && !empty($_POST['Apellido']) &&
+(isset($_POST['Edad']) && !empty($_POST['Edad'])) &&
+(isset($_POST['Telefono']) && !empty($_POST['Telefono']) &&
+(isset($_POST['Gustos']) && !empty($_POST['Gustos']) &&
+(isset($_POST['Ciudad']) && !empty($_POST['Ciudad'])))))))
+{
+$con = new MySQLi("localhost","root","","registro");
+$sql="INSERT INTO registro (Cedula,Nombre,Apellido,Edad,Telefono,Gustos,Ciudad)VALUES('$_POST[Cedula]','$_POST[Nombre]','$_POST[Apellido]','$_POST[Edad]','$_POST[Telefono]','$_POST[Gustos]','$_POST[Ciudad]')";
+$con->query($sql);
+
+?>
