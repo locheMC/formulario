acda8166bf13ed80d017543938c103c84e14b748
CristianJulio <55302815+CristianJulio@users.noreply.github.com>
2020-05-14 15:55:49 -0500

    Update enviar.php

--- enviar.php
+++ enviar.php
@@ -1,11 +1,19 @@
 <?php
+
+$cedula = $_POST['Cedula'];
+$nombre = $_POST['Nombre'];
+$apellido = $_POST['Apellido'];
+$diereccion = $_POST['Direccion'];
+$telefono = $_POST['Selecciones'];
+$sexo = $_POST['Sexo'];
+
  $con = new MySQLi("localhost","root","","registro2");
- $sql = ("insert into registro2(Cedula,Nombre,Apellidos,Direccion,Telefono,Selecciones,Sexo) values($_POST[Cedula],'$_POST[Nombre]','$_POST[Apellidos]','$_POST[Direccion]',$_POST[Telefono],'$_POST[Selecciones]','$_POST[Sexo]')");
+ $sql = ("insert into registro2(Cedula,Nombre,Apellidos,Direccion,Telefono,Selecciones,Sexo) values($cedula,'$nombre','$apellido','$direccion',$telefono,'$sexo')");
  $e = $con->Query($sql);
  if($e) {
    echo "Datos ingresados";
  }else {
-   echo "Datos no ingresados";
+   echo "Datos no ingresados " . $con->connect_errno;
  }
 
  ?>
