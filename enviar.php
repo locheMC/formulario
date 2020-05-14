bc6f067a05b9d47bf163b9601cc9d7490a4e2467
Jose Gonzalez <jagonzalezvillar@gmail.com>
2020-05-14 15:28:23 -0500

    meli

--- enviar.php
+++ enviar.php
@@ -1,5 +1,12 @@
 <?php
-$host = "localhost";
-$user = "root";
-$base = "registro";
-?>
+include("datos.php");
+ $con = new MySQLi("localhost","root","","registro2");
+ $sql = ("insert into registro2 (Cedula,Nombre,Apellidos,Direccion,Telefono,Selecciones,Sexo)VALUES('$_POST[Cedula]','$_POST[Nombre]','$_POST[Apellidos]','$_POST[Direccion]','$_POST[Telefono]','$_POST[Selecciones]','$_POST[Sexo]')");
+ $e = $con->Query($sql);
+ if($e) {
+   echo "Datos ingresados";
+ }else {
+   echo "Datos no ingresados";
+ }
+
+ ?>
